4d4e490388ab0f9e86b0d119921285f40b5f6a41
Alisattar95 <alisattar.alshimmary@gmail.com>
2019-09-16 14:09:28 +0300

    initial commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <share/SharePlugin.h>
+#import <shared_preferences/SharedPreferencesPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTSharePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharePlugin"]];
+  [FLTSharedPreferencesPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTSharedPreferencesPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:notes/services/sharedPref.dart';
+import 'screens/home.dart';
+import 'data/theme.dart';
+
+void main() => runApp(NOTE());
+
+class NOTE extends StatefulWidget {
+  @override
+  _NOTEState createState() => _NOTEState();
+}
+
+class _NOTEState extends State<NOTE> {
+  ThemeData theme = appThemeLight;
+  @override
+  void initState() {
+    super.initState();
+    updateThemeFromSharedPref();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Note App',
+      theme: theme,
+      home: MyHomePage(title: 'Home', changeTheme: setTheme),
+    );
+  }
+
+  setTheme(Brightness brightness) {
+    if (brightness == Brightness.dark) {
+      setState(() {
+        theme = appThemeDark;
+      });
+    } else {
+      setState(() {
+        theme = appThemeLight;
+      });
+    }
+  }
+
+  void updateThemeFromSharedPref() async {
+    String themeText = await getThemeFromSharedPref();
+    if (themeText == 'light') {
+      setTheme(Brightness.light);
+    } else {
+      setTheme(Brightness.dark);
+    }
+  }
+}
